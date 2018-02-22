3c8e0d707b9182572d7ade0007a1022422c94778
Lautaro Angelico <angelico.lautaro@gmail.com>
2018-02-22 00:00:16 -0800

    Removed login module status check

--- modules/social/login/facebook.php
+++ modules/social/login/facebook.php
@@ -15,9 +15,6 @@
 $cfg = loadModuleConfig('login');
 if(!is_array($cfg)) throw new Exception(lang('error_66'));
 
-// login module status
-if(!$cfg['active']) throw new Exception(lang('error_47'));
-
 // social configurations
 $socialCfg = loadConfig('social');
 if(!is_array($socialCfg)) throw new Exception(lang('error_66'));
